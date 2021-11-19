3ec59ece4ddd269fc4c3f47e40d561a21b14d33d
GraceLR <gracerwork08@gmail.com>
2021-11-19 19:02:05 +0000

    v2

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -3,13 +3,23 @@
 // asyncBreeds.js
 const fs = require('fs');
 
-const breedDetailsFromFile = function(breed) {
+// const breedDetailsFromFile = function(breed) {
+//   console.log('breedDetailsFromFile: Calling readFile...');
+//   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+//     console.log("In readFile's Callback: it has the data.");
+//     if (!error) console.log(`Return Value: ${data}`);  // return undefined, data isn't going anywhere, unable to test
+//   });
+// };
+
+const breedDetailsFromFile = function(breed, funcDone) {
   console.log('breedDetailsFromFile: Calling readFile...');
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
     console.log("In readFile's Callback: it has the data.");
-    if (!error) console.log(`Return Value: ${data}`);
+    if (!error) funcDone(data); // flexibility
   });
 };
+
+
 
 module.exports = breedDetailsFromFile;
 
